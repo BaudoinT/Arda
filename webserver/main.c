f1e92505032516b6c4f6edd32d43420d19a6e876
Oscar Baudry <oscar.baudry@etudiant.univ-lille1.fr>
2017-01-26 11:32:53 +0100

    Modif main

--- webserver/main.c
+++ webserver/main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include "socket.h"
+
 int main(int argc,char ** argv){
-/* Arnold Robbins in the LJ of February '95 , describing RCS */
-	if (argc > 1 && strcmp ( argv [1] , "- advice " ) == 0) {
-		printf("Don't Panic !\n");
-		return 42;
+
+	int socket_serv=creer_serveur(8080);
+	if(socket_serv == -1){
+		perror("Erreur lors de la création du serveur")
+		return 0;
 	}
-	printf("Need an advice ?\n");
-	return 0;
+
+	int socket_client;
+	while(1){
+		socket_client=accept(socket_serv, NULL, NULL);
+		if(socket_client==-1){
+			perror("accept");
+			return 0;
+		}
+		const char * message_bienvenue = "Bienvenue sur arda, terre d'Eru";
+		write(socket_client , message_bienvenue , strlen(message_bienvenue));
+	}
 }
