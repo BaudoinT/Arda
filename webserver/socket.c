7c82843a47a5def8534dc80d3644fa908bf1d47b
Baudoin <baudointt@gmail.com>
2017-01-26 11:22:58 +0100

    ajout des fichiers socket.c et socket.h

---
+++ webserver/socket.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "socket.h"
+
+int creer_serveur(int port){
+	
+	int socket_serveur=socket(AF_INET , SOCK_STREAM , 0);
+	if ( socket_serveur == -1){
+		perror ( " socket_serveur " );
+		return -1;
+	} 
+	
+	struct sockaddr_in saddr;
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(8080);
+	saddr.sin_addr.s_addr = INADDR_ANY;
+	
+	if (bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1){
+		perror("bind socket_serveur");
+		return -1;
+	}
+	
+	if (listen(socket_serveur, 10) == -1){
+		perror("listen socket_serveur");
+		return -1;
+	}
+	
+	
+}
