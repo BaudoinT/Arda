44e6709c750ae8ce51b6606a655dc71f67e175a1
Baudoin <baudointt@gmail.com>
2017-02-09 11:35:57 +0100

    Modif socket.c

--- webserver/socket.c
+++ webserver/socket.c
@@ -12,17 +12,18 @@ int creer_serveur(int port){
 		return -1;
 	} 
 	
-	struct sockaddr_in saddr;
-	saddr.sin_family = AF_INET;
-	saddr.sin_port = htons(8080);
-	saddr.sin_addr.s_addr = INADDR_ANY;
-	
 	int optval=1;
 	if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1){
 		perror("Can no set SO_REUSEADDR option");
 		return -1;
 	}
 
+	struct sockaddr_in saddr;
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(8080);
+	saddr.sin_addr.s_addr = INADDR_ANY;
+	
+	
 	if (bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1){
 		perror("bind socket_serveur");
 		return -1;
