e32f649d95b995806396bbb3accbc7b4d29cd146
Baudoin <baudointt@gmail.com>
2017-02-09 11:18:24 +0100

    petite modif

--- webserver/main.c
+++ webserver/main.c
@@ -17,21 +17,21 @@ void initialiser_signaux ( void ){
 
 int main(int argc,char ** argv){
 	initialiser_signaux();
+	
 	int socket_serv=creer_serveur(8080);
 	if(socket_serv == -1){
 		perror("Erreur lors de la création du serveur");
 		return 0;
 	}
-	int socket_client;
 	
+	int socket_client;
 	socket_client=accept(socket_serv, NULL, NULL);
 	if(socket_client==-1){
-	  perror("accept");
+	  perror("Erreur lors de la connection du client");
 	  return 0;
 	}
 
     char* message_bienvenue="Three Rings for the Elven-kings under the sky,\nSeven for the Dwarf-lords in their halls of stone,\nNine for Mortal Men doomed to die,\nOne for the Dark Lord on his dark throne\nIn the Land of Mordor where the Shadows lie.\nOne Ring to rule them all. One Ring to find them,\nOne Ring to bring them all and in the darkness bind them\nIn the Land of Mordor where the Shadows lie.\n\n";
-	
 	sleep(1);
 	write(socket_client , message_bienvenue , strlen(message_bienvenue));
 		
