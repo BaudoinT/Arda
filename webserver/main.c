2f4ce40e2e7afc4ee07e7c9c6087e5fd69634aae
Baudoin <baudointt@gmail.com>
2017-02-10 15:03:12 +0100

    tag 6

--- webserver/main.c
+++ webserver/main.c
@@ -3,8 +3,6 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include "socket.h"
 #include <signal.h>
 #include <sys/wait.h>
@@ -14,7 +12,7 @@ pid_t pid;
 
 void traitement_signal ( int sig ){
 	printf("Signal %d reçu \n",sig );
-	waitpid(pid,NULL,WNOHANG);
+	waitpid(pid,&sig,WNOHANG);
 }
 
 void initialiser_signaux ( void ){
@@ -35,7 +33,7 @@ void initialiser_signaux ( void ){
 
 int main(int argc,char ** argv){
 	initialiser_signaux();
-	
+	int status;
 	int socket_serv=creer_serveur(8080);
 	if(socket_serv == -1){
 		perror("Erreur lors de la création du serveur");
@@ -52,8 +50,6 @@ int main(int argc,char ** argv){
 		}
 
     	char* message_bienvenue="Three Rings for the Elven-kings under the sky,\nSeven for the Dwarf-lords in their halls of stone,\nNine for Mortal Men doomed to die,\nOne for the Dark Lord on his dark throne\nIn the Land of Mordor where the Shadows lie.\nOne Ring to rule them all. One Ring to find them,\nOne Ring to bring them all and in the darkness bind them\nIn the Land of Mordor where the Shadows lie.\n\n";
-		sleep(1);
-	
 
 		if((pid = fork()) == -1) {
 			perror("Erreur lors de la création du processus fils");
@@ -69,7 +65,8 @@ int main(int argc,char ** argv){
 	  			}
 			}
 		}
-		initialiser_signaux();
+		waitpid(pid,&status,WNOHANG);
+
 	}	
 }
 
