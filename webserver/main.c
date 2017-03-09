79008ef77e7cba9389ff92382200ab58348237c1
Baudoin <baudointt@gmail.com>
2017-03-09 11:42:21 +0100

    tag 8

--- webserver/main.c
+++ webserver/main.c
@@ -86,14 +86,15 @@ int main(int argc,char ** argv){
 			perror("Erreur lors de la création du processus fils");
 		}else if(pid==0){			
             char recu[250];
-			fgets(recu,250,discript_socket);
-            if(verif_requete(recu)==-1){
+            fgets(tmp,250,discript_socket);
+            if(verif_requete(recu)==-1)
                 fprintf(discript_socket,"HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n");
-            }else{
+            else{
                 fprintf(discript_socket,"HTTP/1.1 200 OK\r\nConnection: open\r\nContent-Length: 1602\r\n\r\n%s\r\n", message_bienvenue);
             }
+            
             while(fgets(recu,250,discript_socket)!= NULL){
-	  			 if(recu[0]=='\n' || (recu[0]=='\r' && recu[1]=='\n'))
+                if(recu[0]=='\n' || (recu[0]=='\r' && recu[1]=='\n'))
 	  	            printf("%s",recu);
             }
 			close(socket_client);
@@ -102,5 +103,4 @@ int main(int argc,char ** argv){
 		}
 	}	
 }
-
 
