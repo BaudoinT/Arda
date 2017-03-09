eb151828b226209f67d451c8d46a8700e9f33880
Baudoin <baudointt@gmail.com>
2017-03-09 10:26:25 +0100

    modif pour tag 8

--- webserver/main.c
+++ webserver/main.c
@@ -36,60 +36,47 @@ void initialiser_signaux ( void ){
 }
 
 
-char ** split1(char chaine[], int i){
-	char tab[];
-	char del=' ';
-	int cpt =0;
-	while(*chaine != '\0'){
-		if(*chaine==del){
-			cpt++,
-		}
-		*chaine++;
-
-		if(cpt==i){
-			&tab = *chaine;
-		}
-
-	}
-	return tab;
+char *substr(char *src,int pos,int len) { 
+  char *dest=NULL;                        
+  if (len>0) {                            
+    dest = (char *) malloc(len+1);        
+    strncat(dest,src+pos,len);            
+  }
+  return dest;                            
 }
 
 
-int verif_rep_client(char rep[]){
-	char *tab;
-	strcat(tab, rep);
-	char ** sp=split1(tab);
- 	strcmp(*sp,"GET");
- 	printf("%s",rep );
- 	return 0;
- }
+int verif_requete(char *mot){
+	if (strcmp("GET",substr(mot,0,3))){
+		return -1;
+	}
+	if (strcmp("HTTP/1.1",substr(mot,6,8))!=0 && strcmp("HTTP/1.0",substr(mot,6,8))!=0){
+		return -1;
+	}
+	return 0;
+}
 
 int main(int argc,char ** argv){
 
 	
 	// ascii art 
 	int fd  = open("ascii_art",O_RDONLY);
- 	char *message_bienvenue = malloc(3000*sizeof(char));
- 	read(fd,message_bienvenue,3000*sizeof(char));
+ 	char *message_bienvenue = malloc(1600*sizeof(char));
+ 	read(fd,message_bienvenue,1600*sizeof(char));
  	close(fd);
 
-
-
- 	initialiser_signaux();
-	int socket_serv=creer_serveur(8080);
-
-
-	if(socket_serv == -1){
+    initialiser_signaux();
+    int socket_client;
+    int socket_serv=creer_serveur(8080);
+	
+    if(socket_serv == -1){
 		perror("Erreur lors de la création du serveur");
 		return 0;
 	}
-	
-	int socket_client;
 
-	while(1){
+    while(1){
 		socket_client=accept(socket_serv, NULL, NULL);
 		FILE * discript_socket = fdopen(socket_client ,"w+");
-
 		if(socket_client==-1){
 	  		perror("Erreur lors de la connection du client");
 	  		return 0;
@@ -97,24 +84,18 @@ int main(int argc,char ** argv){
 
 		if((pid = fork()) == -1) {
 			perror("Erreur lors de la création du processus fils");
-		}else if(pid==0){
-			fprintf(discript_socket,message_bienvenue);
-	
-			char recu[250];
-			char prompt[]="<Arda> ";
+		}else if(pid==0){			
+            char recu[250];
 			fgets(recu,250,discript_socket);
-
-	  		if(verif_rep_client(recu)==0){
-	  			printf("good \n");
-	  		}
-
-			while(fgets(recu,250,discript_socket)!= NULL){
-	  			
-	  			//fprintf(discript_socket, "%s",prompt );
-	  			//fprintf(discript_socket,recu);
-	  			printf("%s",recu);
-
-			}
+            if(verif_requete(recu)==-1){
+                fprintf(discript_socket,"HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n");
+            }else{
+                fprintf(discript_socket,"HTTP/1.1 200 OK\r\nConnection: open\r\nContent-Length: 1602\r\n\r\n%s\r\n", message_bienvenue);
+            }
+            while(fgets(recu,250,discript_socket)!= NULL){
+	  			 if(recu[0]=='\n' || (recu[0]=='\r' && recu[1]=='\n'))
+	  	            printf("%s",recu);
+            }
 			close(socket_client);
 			return 0;
 
